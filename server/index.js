681d68efc466e387a1fc41f6a33a843e96a2d52a
jesusgonzalezd <jesusgonzalezd18@gmail.com>
2021-04-13 13:21:13 -0400

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,51 @@
+// EXPRESS
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+// Funcion para saber si un nuevo cliente se ha conectado.
+io.on('connection', function(socket){
+    console.log('Nuevo socket conectado');
+});
+
+// Funcion para enviar una peticion http get al servidor con la vista index.
+app.get('/', (req, res) =>{ 
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Funcion de escucha del servidor en port 3000.
+server.listen(3000, function (){
+    console.log("Servidor Escuchando en Puerto", 3000);
+});
+
+// Comunicacion Serial.
+const Serialport = require('serialport');
+const Readline = Serialport.parsers.Readline;
+
+// Inicializa el puerto de comunicacion con arduino.
+const port = new Serialport('COM9', {
+    baudRate: 9600
+});
+
+// Delimita las lecturas de las salidas del arduino.
+const parser = port.pipe(new Readline({ delimeter: '\r\n'}));
+
+// Establece la conexion con arduino.
+parser.on('open', function(){
+    console.log("Conexion abierta");
+});
+
+// Obtiene datos de las salidas del arduino.
+parser.on('data', function(data) {
+    console.log(data);
+    io.emit('arduino:data', data);
+});
+
+//Captura errores de lectura en puerto del arduino.
+port.on('error', function(err){
+    console.log(err);
+});
